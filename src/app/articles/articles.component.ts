99e5a18861cca50e828010d4fe21dab3ee99e635
Daniel Proctor <danielproctor@Daniels-MacBook-Pro.local>
2017-07-29 13:42:35 -0400

    added keystone import

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as keystone from '../../../keystone.js';
+
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss']
 })
 export class ArticlesComponent implements OnInit {
+	articles = [];
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
