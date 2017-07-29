aabcbb18db7498a1aab14690b4526b7b12088086
Daniel Proctor <danielproctor@Daniels-MacBook-Pro.local>
2017-07-29 12:49:27 -0400

    changed primary route to ng4 app

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,7 @@
 var keystone = require('keystone');
 var middleware = require('./middleware');
 var importRoutes = keystone.importer(__dirname);
+var path = require('path');
 
 // Common Middleware
 keystone.pre('routes', middleware.initLocals);
@@ -28,19 +29,21 @@ keystone.pre('render', middleware.flashMessages);
 
 // Import Route Controllers
 var routes = {
-	views: importRoutes('./views'),
+    views: importRoutes('./views'),
 };
 
 // Setup Route Bindings
-exports = module.exports = function (app) {
-	// Views
-	app.get('/', routes.views.index);
-	app.get('/blog/:category?', routes.views.blog);
-	app.get('/blog/post/:post', routes.views.post);
-	app.get('/gallery', routes.views.gallery);
-	app.all('/contact', routes.views.contact);
+exports = module.exports = function(app) {
+    // Views
+    // app.get('/', routes.views.index);
+    app.get('/blog/:category?', routes.views.blog);
+    app.get('/blog/post/:post', routes.views.post);
+    app.get('/gallery', routes.views.gallery);
+    app.all('/contact', routes.views.contact);
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../dist/index.html'));
+    });
 
-	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
-	// app.get('/protected', middleware.requireUser, routes.views.protected);
-
+    // NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
+    // app.get('/protected', middleware.requireUser, routes.views.protected);
 };
