16b2df907c7bd231e15fc02f1349b765430ee7a9
Daniel Proctor <danielproctor@Daniels-MacBook-Pro.local>
2017-07-31 17:01:19 -0400

    Updated post service to use subjects

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,28 +1,41 @@
 import { Injectable, OnInit } from '@angular/core';
-import { Http } from '@angular/http';
+import { Subject } from 'rxjs/Subject';
+import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
-export class PostsService implements OnInit {
-  allArticles: any;
-  reviews: any;
-  essays: any;
-  interviews: any;
+export class PostsService {
+  allArticles;
+  content;
+  dataChanged = new Subject<void>();
 
-  constructor(private http: Http) { }
-
-  ngOnInit() {
-    this.getAllArticles().subscribe(posts => {
-      this.allArticles = posts;
-      this.reviews = posts.filter(post => post.contentType === "book review")
-      this.essays = posts.filter(post => post.contentType === "essay")
-      this.interviews = posts.filter(post => post.contentType === "interview")
-    });
+  constructor(private http: Http) {
+    this.http = http;
+    console.log("post service constructor started");
+    this.fetchArticles();
   }
 
-  getAllArticles() {
-    return this.http.get('/api')
-      .map(res => res.json());
+    // this.getAllArticles().subscribe(posts => {
+    //   this.allArticles = posts;
+
+    //   // this.reviews = posts.filter(post => post.contentType === "book review").slice();
+    //   // this.essays = posts.filter(post => post.contentType === "essay").slice();
+    //   // this.interviews = posts.filter(post => post.contentType === "interview").slice();
+    // });
+
+  fetchArticles() {
+    this.http.get('/api')
+      .map((response: Response) => {
+        return response.json();
+      })
+      .subscribe(
+        (data) => {
+          this.allArticles = data;
+          console.log("I CANT SEE DATA HERE: ", this.allArticles);
+          this.dataChanged.next();
+          return this.allArticles;
+        }
+      );
   }
 
 }
