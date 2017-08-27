386bd01720096429cbf83492810636e83331971d
Daniel Proctor <danielproctor@Daniels-MacBook-Pro.local>
2017-08-27 10:29:10 -0400

    Updated sticky-nav action

--- src/app/sticky-nav/sticky-nav.component.ts
+++ src/app/sticky-nav/sticky-nav.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, Inject, OnInit } from "@angular/core";
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-sticky-nav',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StickNavComponent implements OnInit {
 
-  constructor() { }
+  public navIsFixed: boolean = false;
 
-  ngOnInit() {
-  }
+    constructor(@Inject(DOCUMENT) private document: Document) { }
 
+    ngOnInit() { }
+
+    @HostListener("window:scroll", [])
+    onWindowScroll() {
+      let number = this.document.body.scrollTop;
+      console.log("scrollTop is", number);
+      console.log("main nav info is", this.document.querySelector("main-nav"));
+      if (number > 1000) {
+        this.navIsFixed = true;
+      } else if (this.navIsFixed && number < 10) {
+        this.navIsFixed = false;
+      }
+    }
 }
+
+
+// mainHeader = $('#main-header'),
+// headerOffset = mainHeader.offset().top + mainHeader.outerHeight();
