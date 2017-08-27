39f6dd5a4fcd6353b8551827055c01eb4b5f112d
Daniel Proctor <danielproctor@Daniels-MacBook-Pro.local>
2017-08-27 11:54:52 -0400

    added click actions

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  isFormShown: Boolean = false;
+  displayType: String = 'none';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onShowForm() {
+    console.log("button clicked", this.isFormShown);
+    this.isFormShown = !this.isFormShown;
+  }
+
+  onShowMenu() {
+    if(this.displayType === 'none') {
+      this.displayType = 'block';
+    } else {
+      this.displayType = 'none';
+    }
   }
 
 }
