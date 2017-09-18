2507c421191faca388e5619b2ddc3275ade69572
Daniel Proctor <danielproctor@Daniels-MBP.fios-router.home>
2017-09-17 20:24:08 -0400

    updates to footnotes pipe.

--- src/app/notes.pipe.ts
+++ src/app/notes.pipe.ts
@@ -1,3 +1,4 @@
+import { FootnotesComponent } from './articles/full-article/footnotes/footnotes.component';
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
@@ -10,40 +11,30 @@ export class NotesPipe implements PipeTransform {
   replaceText: string;
   articleText: string;
   space: number;
+  footnotes: string[];
 
   transform(value: any): string {
     this.articleText = value.content.extended.slice();
-    if (this.articleText.indexOf('###')<0) {
+    this.footnotes = value.footnotes;
+    if (this.articleText.indexOf('###') < 0) {
       return this.articleText;
     } else {
-      while (this.articleText.indexOf('###')>0) {
-        this.articleText = this.reformatNotes(this.articleText);
+      while (this.articleText.indexOf('###') > 0) {
+        this.articleText = this.reformatNotes(this.articleText, this.footnotes);
       }
     }
     return this.articleText;
   }
 
-  reformatNotes(text) {
+  reformatNotes(text, notes) {
     this.spot = text.indexOf('###');
     this.space = text.slice(this.spot).indexOf(' ');
     this.noteNumber = text.slice(this.spot + 3, this.spot + this.space);
     this.replaceText = text.slice(this.spot, this.spot + this.space);
-    this.noteTemplate = `
-        <div class="bigfoot-footnote__container">${this.noteNumber}</div>
-    `;
-    console.log('Spot:', this.spot)
-    console.log('Number:',this.noteNumber);
-    console.log('Space:', this.space);
-    console.log('replaceText:', this.replaceText);
-    console.log('Template:', this.noteTemplate);
+    this.noteTemplate = `<div class="bigfoot-footnote__container" id="fnref:${this.noteNumber}"><button class="bigfoot-footnote__button"  alt="See Footnote ${this.noteNumber}" rel="footnote">${this.noteNumber}</button></div>`;
     text = text.replace(this.replaceText,this.noteTemplate);
+    console.log('updated text',text)
     return text;
   }
 
 }
-
-
-// <button class="bigfoot-footnote__button"
-// id="fnref:1" data-footnote-number="1"
-// data-footnote-identifier="1" alt="See Footnote 1" rel="footnote"
-// data-bigfoot-footnote="<p>For example: Leon Trotsky, ‘The Young Turks,’ accessed August 10, 2016, https://www.marxists.org/archive/trotsky/1909/01/1909-turks.htm (Original Kievskaya Mysl, No.3, 3 January 1909 and Rosa Luxemburg, ‘Social Democracy and the National Struggles in Turkey,’ August 10, 2016, https://www.marxists.org/archive/luxemburg/1896/10/10.htm (Original: Sächsische Arbeiter-Zeitung, 8–10 October 1986.)</p>">
